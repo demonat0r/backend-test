4ec7af656b63841e44a2bbc926d14abdc04d3624
demonat0r <lollipop_yum@outlook.com>
2020-08-15 22:58:32 +0300

    first try

---
+++ index.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('form')
+const message = document.querySelector('.alert')
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    fetch('post.php', {
+        method: "POST",
+        body: new FormData(form)
+    })
+        .then(response => response.text())
+        .then((text) =>  {
+            if (text === 'reg succeed') {
+                if (message.classList.contains('alert-danger')) {
+                    message.classList.remove('alert-danger')
+                }
+                    
+                message.classList.add('alert-success')
+
+                message.firstChild.textContent = 'Registration successful'
+                form.hidden = true
+            }    
+            else if (text === 'reg failed') {
+                message.classList.add('alert-danger')
+                message.firstChild.textContent = 'A user with this email address is already registered'
+            }
+        })
+        .catch(error => console.error('Request failed', error ))
+})
+
+
+//проверка полей на фронте или на бэке 
+// <!-- trim -->
---
+++ post.php
@@ -0,0 +1,26 @@
+<?php
+    $users = [
+        "test@mail.ru" => [
+            "NAME" => "test_name_1",
+            "SURNAME" => "test_surname_1",
+            "PASSWORD" => "test_password_1"
+        ],
+        "test@yandex.ru" => [
+            "NAME" => "test_name_2",
+            "SURNAME" => "test_surname_2",
+            "PASSWORD" => "test_password_2"
+        ],
+    ];
+
+    $result_msg;
+
+    if (array_key_exists(trim($_POST['email']), $users)) {
+        $result_msg = "reg failed";
+    } else {
+        $result_msg = "reg succeed";
+    }
+
+    $log = date('Y-m-d H:i:s') . "  " . $result_msg;
+    file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
+
+    echo $result_msg;
